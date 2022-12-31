dc190b3de299b9948f9db0f0b88b2899b72d4076
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-12-31 15:13:52 +1000

    Solve day04 part1

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -1,5 +1,8 @@
 use std::fs;
+use std::str;
 use std::time::Instant;
+
+use md5::{Md5, Digest};
 
 const PROBLEM_NAME: &str = "The Ideal Stocking Stuffer";
 const PROBLEM_INPUT_FILE: &str = "./input/day04.txt";
@@ -39,22 +42,44 @@ pub fn main() {
 }
 
 /// Processes the AOC 2015 Day 04 input file in the format required by the solver functions.
-/// Returned value is ###.
+/// Returned value is String given in the input file.
 fn process_input_file(filename: &str) -> String {
     // Read contents of problem input file
-    let _raw_input = fs::read_to_string(filename).unwrap();
+    let raw_input = fs::read_to_string(filename).unwrap();
     // Process input file contents into data structure
-    unimplemented!();
+    String::from(raw_input.trim())
 }
 
-/// Solves AOC 2015 Day 04 Part 1 // ###
-fn solve_part1(_input: &String) -> String {
-    unimplemented!();
+/// Solves AOC 2015 Day 04 Part 1 // Determines the lowest number that results in an MD5 hash
+/// starting with five zeroes when post-fixed to the secret key.
+fn solve_part1(secret_key: &str) -> u64 {
+    let mut serial: u64 = 1;
+    loop {
+        let hex_result = calculate_md5_hex_result(secret_key, serial);
+        if hex_result.starts_with("00000") {
+            return serial;
+        }
+        serial += 1;
+    }
 }
 
 /// Solves AOC 2015 Day 04 Part 2 // ###
-fn solve_part2(_input: &String) -> String {
-    unimplemented!();
+fn solve_part2(_secret_key: &str) -> u64 {
+    0
+}
+
+/// Calculates the hexadecimal string representation of the MD5 hash from the concatenation of the
+/// secret key and serial number.
+fn calculate_md5_hex_result(secret_key: &str, serial: u64) -> String {
+    let input = format!("{}{}", secret_key, serial);
+    let mut hasher = Md5::new();
+    hasher.update(input.as_bytes());
+    let result = hasher.finalize();
+    let mut hex_output = String::new();
+    for b in result {
+        hex_output.push_str(&format!("{:02x}", b));
+    }
+    hex_output
 }
 
 #[cfg(test)]
@@ -65,17 +90,15 @@ mod test {
     #[test]
     fn test_day04_part1_actual() {
         let input = process_input_file(PROBLEM_INPUT_FILE);
-        let _solution = solve_part1(&input);
-        unimplemented!();
-        // assert_eq!("###", solution);
+        let solution = solve_part1(&input);
+        assert_eq!(254575, solution);
     }
 
     /// Tests the Day 04 Part 2 solver method against the actual problem solution.
     #[test]
     fn test_day04_part2_actual() {
         let input = process_input_file(PROBLEM_INPUT_FILE);
-        let _solution = solve_part2(&input);
-        unimplemented!();
-        // assert_eq!("###", solution);
+        let solution = solve_part2(&input);
+        assert_eq!(1038736, solution);
     }
 }
