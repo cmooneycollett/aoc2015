73f1db96c8c1f4f9acfdac4a91c5886bac6c75b9
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2023-01-01 11:53:56 +1000

    Solve day06 part1

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,9 +1,42 @@
 use std::fs;
 use std::time::Instant;
 
+use fancy_regex::Regex;
+use lazy_static::lazy_static;
+
 const PROBLEM_NAME: &str = "Probably a Fire Hazard";
 const PROBLEM_INPUT_FILE: &str = "./input/day06.txt";
 const PROBLEM_DAY: u64 = 6;
+
+lazy_static! {
+    static ref REGEX_LINE: Regex = Regex::new(r"^(.*) (\d+),(\d+) through (\d+),(\d+)$").unwrap();
+}
+
+/// Represents the limits (inclusive) of the light field affected by a particular instruction.
+struct LightField {
+    min_x: usize,
+    max_x: usize,
+    min_y: usize,
+    max_y: usize,
+}
+
+impl LightField {
+    pub fn new(min_x: usize, max_x: usize, min_y: usize, max_y: usize) -> Self {
+        Self {
+            min_x,
+            max_x,
+            min_y,
+            max_y,
+        }
+    }
+}
+
+/// Represents the different instructions that apply to the light grid.
+enum Instruction {
+    TurnOn { field: LightField },
+    TurnOff { field: LightField },
+    Toggle { field: LightField },
+}
 
 /// Processes the AOC 2015 Day 06 input file and solves both parts of the problem. Solutions are
 /// printed to stdout.
@@ -39,21 +72,79 @@ pub fn main() {
 }
 
 /// Processes the AOC 2015 Day 06 input file in the format required by the solver functions.
-/// Returned value is ###.
-fn process_input_file(filename: &str) -> String {
+/// Returned value is vector of instructions specified by the lines of the input file.
+fn process_input_file(filename: &str) -> Vec<Instruction> {
     // Read contents of problem input file
-    let _raw_input = fs::read_to_string(filename).unwrap();
+    let raw_input = fs::read_to_string(filename).unwrap();
     // Process input file contents into data structure
-    unimplemented!();
+    let mut instructions: Vec<Instruction> = vec![];
+    for line in raw_input.lines() {
+        let line = line.trim();
+        if line.is_empty() {
+            continue;
+        }
+        if let Ok(Some(caps)) = REGEX_LINE.captures(line) {
+            process_regex_captures(caps, &mut instructions);
+        } else {
+            panic!("Day06 - bad format input line: {}", line);
+        }
+    }
+    instructions
 }
 
-/// Solves AOC 2015 Day 06 Part 1 // ###
-fn solve_part1(_input: &String) -> u64 {
-    unimplemented!();
+/// Processes the regex captures and adds the corresponding Instruction to the output record.
+fn process_regex_captures(caps: fancy_regex::Captures, instructions: &mut Vec<Instruction>) {
+    let min_x = caps[2].parse::<usize>().unwrap();
+    let min_y = caps[3].parse::<usize>().unwrap();
+    let max_x = caps[4].parse::<usize>().unwrap();
+    let max_y = caps[5].parse::<usize>().unwrap();
+    let field = LightField::new(min_x, max_x, min_y, max_y);
+    let instruct = match &caps[1] {
+        "turn on" => Instruction::TurnOn { field },
+        "turn off" => Instruction::TurnOff { field },
+        "toggle" => Instruction::Toggle { field },
+        _ => panic!("Day06 - bad instruction: {}", &caps[1]),
+    };
+    instructions.push(instruct);
+}
+
+/// Solves AOC 2015 Day 06 Part 1 // Determines how many lights are left on in the 1000x1000 light
+/// grid after all instructions have been processed (with all lights starting as off).
+fn solve_part1(instructions: &[Instruction]) -> usize {
+    let mut lightgrid: [[bool; 1000]; 1000] = [[false; 1000]; 1000];
+    for instruct in instructions {
+        match instruct {
+            Instruction::TurnOn { field } => {
+                for y in field.min_y..=field.max_y {
+                    for x in field.min_x..=field.max_x {
+                        lightgrid[y][x] = true;
+                    }
+                }
+            }
+            Instruction::TurnOff { field } => {
+                for y in field.min_y..=field.max_y {
+                    for x in field.min_x..=field.max_x {
+                        lightgrid[y][x] = false;
+                    }
+                }
+            }
+            Instruction::Toggle { field } => {
+                for y in field.min_y..=field.max_y {
+                    for x in field.min_x..=field.max_x {
+                        lightgrid[y][x] = !lightgrid[y][x];
+                    }
+                }
+            }
+        }
+    }
+    lightgrid
+        .iter()
+        .map(|line| line.iter().filter(|elem| **elem).count())
+        .sum()
 }
 
 /// Solves AOC 2015 Day 06 Part 2 // ###
-fn solve_part2(_input: &String) -> u64 {
+fn solve_part2(_instructions: &[Instruction]) -> usize {
     unimplemented!();
 }
 
