a381e32e970ada9b31aaceee6f0347aa0c170206
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2023-01-02 15:21:19 +1000

    Solve day09 part1

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -1,6 +1,10 @@
+use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 use std::fs;
 use std::time::Instant;
+
+use fancy_regex::Regex;
+use itertools::Itertools;
 
 const PROBLEM_NAME: &str = "All in a Single Night";
 const PROBLEM_INPUT_FILE: &str = "./input/day09.txt";
@@ -40,22 +44,88 @@ pub fn main() {
 }
 
 /// Processes the AOC 2015 Day 09 input file in the format required by the solver functions.
-/// Returned value is ###.
+/// Returned value is hashmap mapping edge "from" nodes to edge "to" nodes and the associated edge
+/// weight.
 fn process_input_file(filename: &str) -> HashMap<String, HashMap<String, u64>> {
     // Read contents of problem input file
-    let _raw_input = fs::read_to_string(filename).unwrap();
+    let raw_input = fs::read_to_string(filename).unwrap();
     // Process input file contents into data structure
-    unimplemented!();
+    let mut edges: HashMap<String, HashMap<String, u64>> = HashMap::new();
+    let regex_line = Regex::new(r"^([[:alpha:]]+) to ([[:alpha:]]+) = (\d+)$").unwrap();
+    for line in raw_input.lines() {
+        let line = line.trim();
+        if line.is_empty() {
+            continue;
+        }
+        if let Ok(Some(caps)) = regex_line.captures(line) {
+            let node1 = &caps[1];
+            let node2 = &caps[2];
+            let weight = caps[3].parse::<u64>().unwrap();
+            // Forward edge (node1 to node2)
+            add_edges_between_nodes(&mut edges, node1, node2, weight);
+            // Backward edge (node2 to node1)
+            add_edges_between_nodes(&mut edges, node2, node1, weight);
+        } else {
+            panic!("Day 9 - bad format input file // {}", line);
+        }
+    }
+    edges
 }
 
-/// Solves AOC 2015 Day 09 Part 1 // ###
-fn solve_part1(_edges: &HashMap<String, HashMap<String, u64>>) -> u64 {
-    unimplemented!();
+/// Adds a new edge going from node1 to node2 with the given weight.
+fn add_edges_between_nodes(
+    edges: &mut HashMap<String, HashMap<String, u64>>,
+    node1: &str,
+    node2: &str,
+    weight: u64,
+) {
+    if let Entry::Vacant(e) = edges.entry(node1.to_string()) {
+        e.insert(HashMap::from([(node2.to_string(), weight)]));
+    } else {
+        edges
+            .get_mut(&node1.to_string())
+            .unwrap()
+            .insert(node2.to_string(), weight);
+    }
+}
+
+/// Solves AOC 2015 Day 09 Part 1 // Determines the minimum distance required to visit all nodes
+/// in the graph.
+fn solve_part1(edges: &HashMap<String, HashMap<String, u64>>) -> u64 {
+    if let (Some(min), _) = find_min_max_distances_to_visit_all_nodes(edges) {
+        return min;
+    }
+    panic!("Did not find the minimum distance path!");
 }
 
 /// Solves AOC 2015 Day 09 Part 2 // ###
 fn solve_part2(_edges: &HashMap<String, HashMap<String, u64>>) -> u64 {
-    unimplemented!();
+    0
+}
+
+/// Finds the minimum and maximum distances needed to visit all nodes in the graph. Returned value
+/// is a tuple containing the minimum and maximum distances found (in that order).
+fn find_min_max_distances_to_visit_all_nodes(
+    edges: &HashMap<String, HashMap<String, u64>>,
+) -> (Option<u64>, Option<u64>) {
+    let mut min_dist: Option<u64> = None;
+    let mut max_dist: Option<u64> = None;
+    // Try each of the possible orders to visit all nodes
+    for nodes in edges.keys().permutations(edges.len()) {
+        // Calculate the total distance between the nodes in the order visited
+        let mut dist = 0;
+        for i in 1..nodes.len() {
+            dist += edges.get(nodes[i - 1]).unwrap().get(nodes[i]).unwrap();
+        }
+        // Check if a new minimum or maximum distance has been found
+        if min_dist.is_none() || dist < min_dist.unwrap() {
+            min_dist = Some(dist);
+        }
+        if max_dist.is_none() || dist > max_dist.unwrap() {
+            max_dist = Some(dist);
+        }
+    }
+    (min_dist, max_dist)
 }
 
 #[cfg(test)]
