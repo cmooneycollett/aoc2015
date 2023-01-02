f9406c6489f78820dcde361582881de8d3a3f132
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2023-01-02 21:18:14 +1000

    Solve day11 part1

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,9 +1,71 @@
 use std::fs;
 use std::time::Instant;
 
+use fancy_regex::Regex;
+use lazy_static::lazy_static;
+
 const PROBLEM_NAME: &str = "Corporate Policy";
 const PROBLEM_INPUT_FILE: &str = "./input/day11.txt";
 const PROBLEM_DAY: u64 = 11;
+
+lazy_static! {
+    static ref REGEX_TRIPLE: Regex =
+        Regex::new(r"(abc|bcd|cde|def|efg|fgh|pqr|qrs|rst|stu|tuv|uvw|vwx|wxy|xyz)").unwrap();
+    static ref REGEX_PAIR: Regex = Regex::new(r"([a-z])\1.*([a-z])\2").unwrap();
+    static ref REGEX_BAD: Regex = Regex::new(r"(i|l|o)").unwrap();
+}
+
+/// Password struct used to generate valid passwords based on the correctness rules.
+pub struct Password {
+    chars: Vec<char>
+}
+
+impl Password {
+    pub fn new(chars: &Vec<char>) -> Password {
+        Password { chars: chars.to_vec() }
+    }
+}
+
+impl Iterator for Password {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.chars.is_empty() {
+            self.chars = vec!['a'];
+        }
+        loop {
+            let mut i = self.chars.len() - 1;
+            // Increment chars to find the next candidate password
+            loop {
+                // Increment password
+                if self.chars[i] != 'z' {
+                    self.chars[i] = char::from(self.chars[i] as u8 + 1);
+                    break;
+                } else {
+                    self.chars[i] = 'a';
+                    if i == 0 {
+                        self.chars.insert(0, 'a');
+                        break;
+                    } else {
+                        i -= 1;
+                    }
+                }
+            }
+            // Check if password is valid
+            let candidate = self.chars.iter().collect::<String>();
+            if let Ok(Some(caps)) = REGEX_PAIR.captures(&candidate) {
+                if caps[1] == caps[2] {
+                    continue;
+                }
+            } else {
+                continue;
+            }
+            if REGEX_TRIPLE.is_match(&candidate).unwrap() && !REGEX_BAD.is_match(&candidate).unwrap() {
+                return Some(candidate);
+            }
+        }
+    }
+}
 
 /// Processes the AOC 2015 Day 11 input file and solves both parts of the problem. Solutions are
 /// printed to stdout.
@@ -42,19 +104,20 @@ pub fn main() {
 /// Returned value is ###.
 fn process_input_file(filename: &str) -> String {
     // Read contents of problem input file
-    let _raw_input = fs::read_to_string(filename).unwrap();
+    let raw_input = fs::read_to_string(filename).unwrap();
     // Process input file contents into data structure
-    unimplemented!();
+    raw_input.trim().to_string()
 }
 
-/// Solves AOC 2015 Day 11 Part 1 // ###
-fn solve_part1(_input: &String) -> String {
-    unimplemented!();
+/// Solves AOC 2015 Day 11 Part 1 // Gets the next valid password from the current seed password.
+fn solve_part1(seed: &String) -> String {
+    let mut password_gen = Password::new(&seed.chars().collect::<Vec<char>>());
+    password_gen.next().unwrap()
 }
 
 /// Solves AOC 2015 Day 11 Part 2 // ###
-fn solve_part2(_input: &String) -> String {
-    unimplemented!();
+fn solve_part2(_seed: &String) -> String {
+    String::from("-1")
 }
 
 #[cfg(test)]
